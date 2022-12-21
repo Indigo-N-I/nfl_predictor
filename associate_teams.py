6a2f68ed1425f02807ffa471945f65d6f9c35830
Tim Huang <readatontim@gmail.com>
2022-12-20 20:13:45 -0800

    change by teams

---
+++ associate_teams.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def main():
+    history = pd.read_csv('history_no_nan.csv')
+    teams = pd.read_csv('nfl_teams.csv')
+
+    for team in set(teams['team_name']):
+        # print(history['team_home'] == team)
+        team_history = history[(history['team_home'] == team) | (history['team_away'] == team)]
+        # break
+        team_history.to_csv(f'{team}.csv')
+
+if __name__ == '__main__':
+    main()
